d8d14c6aa572feb9b7dcd2bab615618e18a1aa7b
Al_Gerstner <waxing-moon@mail.ru>
2022-09-30 21:08:53 +0700

    Программы для задач 25, 27, 29

--- lesson_4/Program.cs
+++ lesson_4/Program.cs
@@ -14,7 +14,29 @@ Console.WriteLine("A в степени B = " + result);
 */
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
 Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
+int m = 0;
+while (n > 0)
+{
+int x = n % 10;
+n = n / 10;
+m = m + x;
+}
+Console.WriteLine("сумма цифр числа: " + m);
+*/     
+// Программа считает только в положительных числах.
+// Подозреваю, что нужно прописать команду которая будет считать не обращая внимания на знак, но не знаю как)
 
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+/*
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+*/
