2618caaef1bd22e7ced07583863663316bfcefc2
Al_Gerstner <waxing-moon@mail.ru>
2022-09-30 21:08:12 +0700

    Программы для задач 25, 27, 29

---
+++ lesson_4/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+Console.Clear();
+Console.Write("Введите первое число (A): ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число (B): ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = A;
+for (int i = 1; i < B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine("A в степени B = " + result);
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
