a73bb1a20d8ee7f5a1cb8becbd0ac3f7ca14bc1b
Al_Gerstner <waxing-moon@mail.ru>
2022-10-15 09:09:21 +0700

    lesson_5

--- lesson_5/Program.cs
+++ lesson_5/Program.cs
@@ -1,14 +1,93 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
+/*
+Console.Clear();
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+int count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count ++;
+    }
 
+    return count;
+}
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine($"колличество четных чисел равно {count(array)}");
+*/
 
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
-
-
+/*
+Console.Clear();
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 10);
+}
+int Index(int[] array)
+{
+    int Index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] % 2 != 0)
+            Index += i;
+    }
+// не соображу как исправить чтоб считало сумму элементов
+    return Index;
+}
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine($"сумма элементов нечетных позиций равна {Index(array)}");
+*/
 
 // Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементом массива.
-
-
+/*
+Console.Clear();
+void Array(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 100);
+}
+int min(int[] array)
+{
+    int mini = 100;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < mini)
+            mini = array[i];
+    }
+    return mini;
+}
+int max(int[] array)
+{
+    int maxi = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > maxi)
+            maxi = array[i];
+    }
+    return maxi;
+}
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Array(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine($"разница: {max(array) - min(array)}");
+*/
--- lesson_6/Program.cs
+++ lesson_6/Program.cs
@@ -1,13 +1,13 @@
 ﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3*/
-/*
+
 Console.Clear();
 Console.Write("Введите кол-во элементов массива: ");
 int M = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[M];
-Console.Write("Введите числа: ");
-int[] arr = (Console.ReadLine());
+Console.WriteLine("Введите числа: ");
+int arr = Convert.ToInt32(Console.ReadLine());
 int count = 0;
 for (int i = 0; i < array.Length; i++)
 {
@@ -17,7 +17,7 @@ for (int i = 0; i < array.Length; i++)
     }
 }
 Console.WriteLine("Кол-во элементов > 0: " + count);
-*/
+
 
 
 
